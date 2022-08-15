9998388517b6e82b757a41380be7fe04b5a34f7c
rayoz12 <rayoz12@yahoo.com.au>
2022-08-15 22:05:55 +1000

    inital commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# syntax=docker/dockerfile:1
+
+FROM python:alpine
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+COPY . .
+
+EXPOSE 3000
+
+CMD [ "python3", "main.py"]
---
+++ main.py
@@ -0,0 +1,78 @@
+import time
+from os import environ
+import ssl
+from prometheus_client import start_http_server
+from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily, REGISTRY
+import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+androbd_topic = environ.get("ANDROBD_TOPIC", None)
+mqtt_host = environ.get("MQTT_HOST", None)
+mqtt_port = environ.get("MQTT_PORT", None)
+mqtt_is_ws = "MQTT_WS_ON" in environ
+mqtt_ws_path = environ.get("MQTT_WS_PATH", "/mqtt")
+mqtt_is_tls = "MQTT_TLS_ON" in environ
+mqtt_tls_ca_cert = environ.get("MQTT_TLS_CA_CERT", None)
+mqtt_tls_certfile = environ.get("MQTT_TLS_CERTFILE", None)
+mqtt_tls_keyfile = environ.get("MQTT_TLS_KEYFILE", None)
+# see https://docs.python.org/3/library/ssl.html#ssl.CERT_NONE. Defaults to "FALSE", can be set to "TRUE"
+mqtt_tls_ignore_invalid_certs = environ.get("MQTT_IGNORE_INVALID_CERTS", "FALSE")
+mqtt_tls_ignore_invalid_certs = mqtt_tls_ignore_invalid_certs == "TRUE"
+
+mqtt_username = environ.get("MQTT_USERNAME", None)
+mqtt_password = environ.get("MQTT_PASSWORD", None)
+
+required = [androbd_topic, mqtt_host, mqtt_port]
+if None in required:
+    raise "Required options not passed in env"
+
+last_data = {}
+is_up = True
+
+class AndroOBDCollector(object):
+    def collect(self):
+        yield GaugeMetricFamily('androbd_up', "If androbd is submitting data", value=1 if is_up else 0)
+        for key in last_data:
+            yield GaugeMetricFamily(f"androbd_{key}", f'androbd data: {key}', value=last_data[key])
+
+
+def on_connect(client, userdata, flags, rc):  # The callback for when 
+    # the client connects to the broker 
+    print("Connected with result code {0}".format(str(rc)))  
+
+    client.subscribe(f"{androbd_topic}/#")
+
+
+
+def on_message(client, userdata, msg):  
+    print("Message received-> " + msg.topic + " " + str(msg.payload))  # Print a received msg
+    split_topic = msg.topic.split("/")
+    sensor = split_topic[-1]
+    last_data[sensor] = msg.payload.decode("utf-8")
+    # print(split_topic, sensor, last_data[sensor])
+
+if __name__ == '__main__':
+    REGISTRY.register(AndroOBDCollector())
+    start_http_server(3000)
+
+    client = mqtt.Client("androbd-exporter", transport = "websockets" if mqtt_is_ws else "tcp")  # Create instance of client
+    client.on_connect = on_connect  # Define callback function for successful connection
+    client.on_message = on_message  # Define callback function for receipt of a message
+    
+    if mqtt_is_ws:
+        client.ws_set_options(path=mqtt_ws_path)
+
+    if mqtt_is_tls:
+        client.tls_set(mqtt_tls_ca_cert, mqtt_tls_certfile, mqtt_tls_keyfile, ssl.CERT_NONE if mqtt_tls_ignore_invalid_certs else ssl.CERT_REQUIRED)
+        if mqtt_tls_ignore_invalid_certs:
+            client.tls_insecure_set(true)
+
+    client.username_pw_set(mqtt_username, mqtt_password)
+
+    client.connect(mqtt_host, int(mqtt_port))
+    client.loop_forever()  # Start networking daemon
+    
+    # while True:
+    #     time.sleep(30)
